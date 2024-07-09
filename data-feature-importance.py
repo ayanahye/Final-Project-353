853763d5fa934168e32e30967e1a77f8cc1bd25b
ayana <ayanahye2@gmail.com>
2024-07-09 15:22:55 -0700

    Finish feature importance ranking with random forest regressor

--- data-feature-importance.py
+++ data-feature-importance.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import MinMaxScaler
 import seaborn as sns
+from sklearn.pipeline import make_pipeline
 
 data = pd.read_csv("merged_data_imputed.csv")
 print(data.head(20))
@@ -26,11 +27,6 @@ sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1, center=0)
 plt.title('Correlation Heatmap')
 plt.savefig("Correlation_Heatmap.png")
 
-
-# Impute missing values in X for whatever model
-imputer = SimpleImputer(strategy='mean')
-X_imputed = imputer.fit_transform(X)
-
 # check if assumptions for multiple linear regression is satisified
     # not satisified
 for column in X.columns:
@@ -41,7 +37,43 @@ for column in X.columns:
     plt.title(f'Life Expectancy vs {column}')
     plt.savefig(f"visualizations/life_expect_vs_feature/Life_vs_{column}")
 
-X_train, X_valid, y_train, y_valid = train_test_split(X_imputed, y, test_size=0.2, random_state=42)
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # somewhat of a linear relationship for Life Expectancy vs Life Ladder and social support but not for other variables
+
+# dont need to scale features for random forest but potentially need to if switching the model to something else
+rf_model = make_pipeline(
+    SimpleImputer(strategy='mean'),
+    MinMaxScaler(),
+    RandomForestRegressor(n_estimators=100, random_state=42)
+)
+
+rf_model.fit(X_train, y_train)
+
+y_pred = rf_model.predict(X_valid)
+
+mse = mean_squared_error(y_valid, y_pred)
+
+r2 = r2_score(y_valid, y_pred)
+
+print(f"Mean Squared Error: {mse}")
+print(f"R-squared Score: {r2}")
+
+# feature_importances_
+# the higher the more important the feature
+# computed as the normalized total reduction of the criterion brought by that feature
+# also called Gini importance
+# source: https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestRegressor.html
+
+rf_model = rf_model.named_steps['randomforestregressor']
+
+importances = pd.DataFrame({'feature': X.columns, 'importance': rf_model.feature_importances_})
+importances = importances.sort_values('importance', ascending=False)
+print(importances)
+
+# life ladder importance is high, could suggest that the country's overall life ladder score is strongly associated with life expectancy
+
+
+
+
 
